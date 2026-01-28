cb331d564abcd7c6db2e07548f926c4d838b8f26
Illia <prikhodko.illia@gmail.com>
2026-01-28 19:15:46 +0100

    implemented buttons

--- concept.py
+++ concept.py
@@ -1,159 +1,128 @@
 import random
 import tkinter as tk
-from tkinter import ttk, StringVar
-
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-values = {
-    "2": 2,
-    "3": 3,
-    "4": 4,
-    "5": 5,
-    "6": 6,
-    "7": 7,
-    "8": 8,
-    "9": 9,
-    "10": 10,
-    "J": 10,
-    "Q": 10,
-    "K": 10,
-}
-
-# TODO:
-# Implement the suits
-suits = []
+from tkinter import ttk, StringVar, IntVar
+from players import Player, DealerHand, Hand
 
 
-def hand_total(cards):
-    if "A" in cards:
-        # There can only be one soft Ace in the hand
-        # Since two soft Aces add to 22 and bust
-        n_aces = cards.count("A")
-        sum_not_aces = sum([values[card] for card in cards if card != "A"])
-        return (n_aces + sum_not_aces, 10 + n_aces + sum_not_aces)
-
-    return sum([values[card] for card in cards])
-
-
-class Hand:
-    def __init__(self):
-        self.cards = [random.choice(ranks) for _ in range(2)]
-        self.value = hand_total(self.cards)
-
-    def hit(self):
-        # Can only hit if not busted
-        if "A" in self.cards and self.value[0] >= 21:
-            return
-        elif self.value >= 21:
-            return
-
-        new_card = random.choice(ranks)
-        self.cards.append(new_card)
-        self.value = hand_total(self.cards)
-
-        player_hand_str.set(f"Player {self.cards}")
+def stand(player: Player, dealer_hand: DealerHand):
+    if player.can_finish():
+        dealer_hand.finish()
+        dealer_hand_str.set(f"Dealer {dealer_hand.cards}")
+        result_str.set(
+            f"""
+            Previous: Player {player.get_totals()} vs Dealer {
+                dealer_hand.get_total()
+            }
+            """
+        )
+        player.resolve(dealer_hand)
+        wpl_str.set(f"W/P/L\n{player.wins}/{player.pushes}/{player.losses}")
+        disable_all()
+        root.after(2000, lambda: reset(player, dealer_hand))
+    else:
+        player.next()
 
 
-class Dealer:
-    def __init__(self, rule="H17"):
-        self.cards = [random.choice(ranks)]
-        self.value = hand_total(self.cards)
-        self.rule = rule
-
-    def hit(self):
-        new_card = random.choice(ranks)
-        self.cards.append(new_card)
-        self.value = hand_total(self.cards)
-
-        dealer_hand_str.set(f"Dealer {self.cards}")
-
-    def finish(self):
-        self.hit()
-        while self.dealer_hitting_condition(dealer_hand, self.rule):
-            self.hit()
-
-    def dealer_hitting_condition(self):
-        is_ace_in_hand = isinstance(self.cards, tuple)
-
-        if is_ace_in_hand:
-            soft_total = self.value[1]
-            hard_total = self.value[0]
-        else:
-            # HACK:
-            # If no ace in the hand, there is no soft total
-            soft_total = -1
-            hard_total = self.value
-
-        if self.rule == "S17":
-            return not (
-                (soft_total >= 17 and soft_total <= 21) or hard_total >= 17
-            )
-        if self.rule == "H17":
-            return not (
-                (soft_total > 17 and soft_total <= 21) or hard_total >= 17
-            )
+def hit(player: Player, dealer_hand: DealerHand):
+    player.active_hand().hit()
+    player_hand_str.set(f"Player {player.show_hands()}")
+    if player.active_hand().is_busted():
+        stand(player, dealer_hand)
 
 
-def hit(hand):
-    # Can only hit if not busted
-    if "A" in hand[0] and hand[1][0] >= 21:
-        return hand
-    elif hand[1] >= 21:
-        return hand
-
-    new_card = random.choice(ranks)
-    new_cards = hand[0]
-    new_cards.append(new_card)
-
-    new_hand = (new_cards, hand_total(new_cards))
-    player_hand_str.set(f"Player {new_cards}")
-    return new_hand
+def double(player: Player, dealer_hand: DealerHand):
+    player.active_hand().hit()
+    player_hand_str.set(f"Player {player.show_hands()}")
+    stand(player, dealer_hand)
 
 
-def stand():
-    pass
+def split(player: Player, dealer_hand: DealerHand):
+    player.split()
+    player_hand_str.set(f"Player {player.show_hands()}")
 
 
-def double():
-    pass
+def surrender(player: Player, dealer_hand: DealerHand):
+    # for now equivalent to standing
+    stand(player, dealer_hand)
 
 
-def split():
-    pass
+def reset(player: Player, dealer_hand: DealerHand):
+    player.reset()
+    dealer_hand.reset()
+    player_hand_str.set(f"Player {player.show_hands()}")
+    dealer_hand_str.set(f"Dealer {dealer_hand.cards}")
+    enable_all()
 
 
-def surrender():
-    pass
+def disable_all():
+    hit_button.config(state=tk.DISABLED)
+    stand_button.config(state=tk.DISABLED)
+    double_button.config(state=tk.DISABLED)
+    surrender_button.config(state=tk.DISABLED)
+    split_button.config(state=tk.DISABLED)
+
+
+def enable_all():
+    hit_button.config(state=tk.NORMAL)
+    stand_button.config(state=tk.NORMAL)
+    double_button.config(state=tk.NORMAL)
+    surrender_button.config(state=tk.NORMAL)
+    split_button.config(state=tk.NORMAL)
 
 
 if __name__ == "__main__":
     root = tk.Tk()
 
-    player_hand = Hand()
-    dealer_hand = Dealer()
+    player = Player(init_hand=Hand(["J", "Q"]))
+    dealer_hand = DealerHand.deal_initial()
 
     player_hand_str = StringVar()
-    player_hand_str.set(f"Player {player_hand.cards}")
+    player_hand_str.set(f"Player {player.show_hands()}")
 
     dealer_hand_str = StringVar()
     dealer_hand_str.set(f"Dealer {dealer_hand.cards}")
 
-    dealer_label = ttk.Label(root, textvariable=dealer_hand_str).pack()
-    player_label = ttk.Label(root, textvariable=player_hand_str).pack()
+    dealer_label = ttk.Label(root, textvariable=dealer_hand_str)
+    dealer_label.pack()
+    player_label = ttk.Label(root, textvariable=player_hand_str)
+    player_label.pack()
 
     # Count the player total, finish the dealer
-    stand_button = ttk.Button(root, text="Stand", command=stand).pack()
+    stand_button = ttk.Button(
+        root, text="Stand", command=lambda: stand(player, dealer_hand)
+    )
+    stand_button.pack()
 
-    hit_button = ttk.Button(root, text="Hit", command=player_hand.hit).pack()
+    hit_button = ttk.Button(
+        root, text="Hit", command=lambda: hit(player, dealer_hand)
+    )
+    hit_button.pack()
 
     # Hit one card, count total, finish the dealer
-    double_button = ttk.Button(root, text="Double", command=double).pack()
+    double_button = ttk.Button(
+        root, text="Double", command=lambda: double(player, dealer_hand)
+    )
+    double_button.pack()
 
     # Create two hands and keep the play sequential
-    split_button = ttk.Button(root, text="Split", command=split).pack()
+    split_button = ttk.Button(
+        root, text="Split", command=lambda: split(player, dealer_hand)
+    )
+    split_button.pack()
 
     # Deal no cards, finish the dealer
     surrender_button = ttk.Button(
-        root, text="Surrender", command=surrender
-    ).pack()
+        root, text="Surrender", command=lambda: surrender(player, dealer_hand)
+    )
+    surrender_button.pack()
+
+    result_str = StringVar()
+    result_label = tk.Label(root, textvariable=result_str)
+    result_label.pack()
+
+    wpl_str = StringVar()
+    wpl_str.set("W/P/L\n0/0/0")
+    wpl_label = tk.Label(root, textvariable=wpl_str)
+    wpl_label.pack()
 
     root.mainloop()
---
+++ players.py
@@ -0,0 +1,203 @@
+import random
+
+# TODO:
+# For every move we need to compare it with the strategy chart
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+values = {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "10": 10,
+    "J": 10,
+    "Q": 10,
+    "K": 10,
+}
+
+# TODO:
+# Implement the suits
+suits = []
+
+
+def update_total(cards):
+    if "A" in cards:
+        # There can only be one soft Ace in the hand
+        # Since two soft Aces add to 22 and bust
+        n_aces = cards.count("A")
+        sum_not_aces = sum([values[card] for card in cards if card != "A"])
+        return (n_aces + sum_not_aces, 10 + n_aces + sum_not_aces)
+
+    return sum([values[card] for card in cards])
+
+
+def _get_total(hand):
+    if "A" in hand.cards:
+        hard_total = hand.value[0]
+        soft_total = hand.value[1]
+        return hard_total * (soft_total > 21) + soft_total * (soft_total <= 21)
+    else:
+        return hand.value
+
+
+class Hand:
+    def __init__(self, cards, rule="H17"):
+        self.cards = cards
+        self.rule = rule
+        self.recompute()
+
+    def recompute(self):
+        self.value = update_total(self.cards)
+        self.total = _get_total(self)
+        self.busted = False
+
+    @classmethod
+    def deal_initial(cls, rule="H17"):
+        return cls([random.choice(ranks) for _ in range(2)], rule)
+
+    def bust_update(self):
+        if self.total > 21:
+            self.busted = True
+
+    def is_busted(self):
+        return self.busted
+
+    def get_total(self):
+        return self.total
+
+    def hit(self):
+        # Can only hit if not busted
+        if self.busted:
+            return
+
+        new_card = random.choice(ranks)
+        self.cards.append(new_card)
+        self.value = update_total(self.cards)
+        self.total = _get_total(self)
+        self.bust_update()
+
+
+class DealerHand(Hand):
+    @classmethod
+    def deal_initial(cls, rule="H17"):
+        return cls([random.choice(ranks)], rule)
+
+    def reset(self):
+        self.cards = [random.choice(ranks)]
+        self.recompute()
+
+    def dealer_hitting_condition(self):
+        is_ace_in_hand = isinstance(self.value, tuple)
+
+        if is_ace_in_hand:
+            soft_total = self.value[1]
+            hard_total = self.value[0]
+        else:
+            soft_total = -1
+            hard_total = self.value
+
+        if self.rule == "S17":
+            return not (
+                (soft_total >= 17 and soft_total <= 21) or hard_total >= 17
+            )
+        if self.rule == "H17":
+            return not (
+                (soft_total > 17 and soft_total <= 21) or hard_total >= 17
+            )
+
+    def finish(self):
+        while self.dealer_hitting_condition():
+            self.hit()
+
+
+class Player:
+    def __init__(self, init_hand=None):
+        if init_hand is None:
+            self.hands: list[Hand] = [Hand.deal_initial()]
+        else:
+            self.hands: list[Hand] = [init_hand]
+
+        self.wins = 0
+        self.losses = 0
+        self.pushes = 0
+
+        self.active = 0
+
+    def reset(self):
+        self.hands: list[Hand] = [Hand.deal_initial()]
+        self.active = 0
+
+    def show_hands(self):
+        if len(self.hands) == 1:
+            hands = f"{self.hands[0].cards}"
+        else:
+            hands = f"{[hand.cards for hand in self.hands]}"
+        return hands
+
+    def get_totals(self):
+        if len(self.hands) == 1:
+            total = self.hands[0].get_total()
+        else:
+            total = [hand.get_total() for hand in self.hands]
+        return total
+
+    def active_hand(self):
+        return self.hands[self.active]
+
+    def next(self):
+        self.active += 1
+
+    def resolve(self, dealer: DealerHand):
+        for hand in self.hands:
+            is_win = win(hand, dealer)
+            is_push = push(hand, dealer)
+            if is_win:
+                self.wins += 1
+            elif is_push:
+                self.pushes += 1
+            else:
+                self.losses += 1
+
+    def split(self):
+        active_hand = self.active_hand()
+        if len(active_hand.cards) != 2:
+            return
+
+        are_tens = active_hand.cards[0] in [
+            "J",
+            "Q",
+            "K",
+            "10",
+        ] and active_hand.cards[1] in ["J", "Q", "K", "10"]
+
+        if not are_tens and active_hand.cards[0] != active_hand.cards[1]:
+            # TODO: Let the player know that they can't split
+            return
+
+        card = active_hand.cards[1]
+        active_hand.cards.pop(1)
+        self.hands.insert(self.active + 1, Hand([card]))
+        for i in range(self.active, self.active + 2):
+            self.hands[i].hit()
+
+    def can_finish(self):
+        if len(self.hands) == 1 or self.active == len(self.hands) - 1:
+            return True
+        else:
+            return False
+
+
+def win(player_hand: Hand, dealer_hand: DealerHand):
+    return (
+        player_hand.get_total() > dealer_hand.get_total()
+        and not player_hand.is_busted()
+    ) or (not player_hand.is_busted() and dealer_hand.is_busted())
+
+
+def push(player_hand: Hand, dealer_hand: DealerHand):
+    return player_hand.get_total() == dealer_hand.get_total() and (
+        not player_hand.is_busted() and not dealer_hand.is_busted()
+    )
